440908a41d64e9bc09f3961e743cff5dc9d1b319
Sebastian <gutierrez.sebastian6@gmail.com>
2021-08-10 09:54:24 -0300

    github page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'generadorDeUruguayo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('generadorDeUruguayo');
+    expect("Uruguayan generator").toEqual('generadorDeUruguayo');
   });
 
   it('should render title', () => {
